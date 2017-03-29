69712f055a26941405bfa01660c078e37ef08330
caycay <carolyynka@gmail.com>
2017-03-29 12:45:17 +0200

    third player

---
+++ ThirdPerson/Assets/TriggerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerController : MonoBehaviour {
+
+    public GameObject LightG;
+
+    void OnTriggerEnter(Collider other)
+
+    {
+
+        if (other.GetComponent<Collider>().CompareTag("Player"))
+
+        {
+
+            LightG.GetComponent<Light>().color = Color.blue;
+
+            AudioSource sound = other.GetComponent<AudioSource>();
+
+            sound.Play();
+
+        }
+
+    }
+}
